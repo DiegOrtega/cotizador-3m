f0fd283b6b2fdd2d45cb400f41d270971b3ca349
DiegOrtega <nemesisrda@gmail.com>
2017-02-12 11:03:47 -0600

    database

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var neo4j = require('neo4j-driver').v1;
-//var validator = require('validator'); 
-//var reload = require('reload');
-//var timeout = require('connect-timeout'); //express v4
 
 var app = express(); 
 
@@ -17,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-//Conexión con base de datos remota
+/* Conexión con base de datos remota
 var graphenedbURL = process.env.GRAPHENEDB_MAROON_BOLT_URL;
 var graphenedbUser = process.env.GRAPHENEDB_MAROON_BOLT_USER;
 var graphenedbPass = process.env.GRAPHENEDB_MAROON_BOLT_PASSWORD;
@@ -25,7 +22,10 @@ var graphenedbPass = process.env.GRAPHENEDB_MAROON_BOLT_PASSWORD;
 //Protocolo de conexión para servidor cloud heroku
 var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
 
-//var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'Sistemas'));
+*/
+
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'Sistemas'));
+
 
 var session = driver.session();
 

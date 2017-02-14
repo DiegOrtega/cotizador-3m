98002e66e2fca7b549d487bd5bda1d69b80eefc9
DiegOrtega <nemesisrda@gmail.com>
2017-02-14 17:34:40 -0600

    botones de productos

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+/*
 // Conexión con base de datos remota
 var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
 var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
@@ -27,10 +27,10 @@ var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
 
 //Protocolo de conexión para servidor cloud heroku
 var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
+*/
 
 
-
-//var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'Sistemas'));
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'Sistemas'));
 
 var session = driver.session();
 
